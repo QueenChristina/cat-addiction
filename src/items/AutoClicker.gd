db4f16cd366aa0eedc89b23fdebe65188ab72b31
Cole Kissane <coler706@gmail.com>
2021-11-19 17:55:44 -0800

    feat(auto-clicker): animations

--- src/items/AutoClicker.gd
+++ src/items/AutoClicker.gd
@@ -3,8 +3,16 @@ extends AnimatedSprite
 # TODO: make
 
 # Called when the node enters the scene tree for the first time.
+var t=0
 func _ready():
 	pass # Replace with function body.
-#
-#func _on_Timer_timeout():
-#	Globals.score += 1
+func _process(delta):
+	t+=delta
+	var n=get_node("../Cat")
+	var dis=sin(10*t)*10+30
+	scale=Vector2(1,1)*(3-sin(10*t))/10
+	rotation=t-PI/4
+	position=Vector2(cos(t)*dis,sin(t)*dis)+n.position
+
+func _on_Timer_timeout():
+	Globals.score += 1
