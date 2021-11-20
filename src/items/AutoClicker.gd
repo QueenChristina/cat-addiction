4d351073cc6359d4f311b3a1a563a717b740a2ad
Cole Kissane <coler706@gmail.com>
2021-11-19 18:32:21 -0800

    feat(auto-clicker): clicky cat hz

--- src/items/AutoClicker.gd
+++ src/items/AutoClicker.gd
@@ -3,16 +3,20 @@ extends AnimatedSprite
 # TODO: make
 
 # Called when the node enters the scene tree for the first time.
-var t=rand_range(0,2*PI)
+var click_hz=2
+var t=rand_range(0,1)
+var last_click_t=t+0
 func _ready():
+	self.last_click_t=t+0
 	pass # Replace with function body.
 func _process(delta):
-	if(floor((t/(2*PI))*10)!=floor(((t+delta)/(2*PI))*10)):
-		var n=get_node("../Cat")
-		n.clicky(n.to_local(self.to_global(Vector2(0,0))))
+	var cat=get_node("../Cat")
 	t+=delta
-	var n=get_node("../Cat")
-	var dis=-cos(10*t)*10+30
-	scale=Vector2(1,1)*(3+cos(10*t))/10
+	if t-last_click_t>1.0/click_hz:
+		self.last_click_t=self.last_click_t+1.0/click_hz
+		cat.clicky(cat.to_local(self.to_global(Vector2(0,0))))
+	var click_ler=(t-last_click_t)*click_hz
+	var radius=-cos(click_ler*2*PI)*10+30
+	scale=Vector2(1,1)*(3+cos(click_ler*2*PI))/10
 	rotation=t-PI/4
-	position=Vector2(cos(t)*dis,sin(t)*dis)+n.position
+	position=Vector2(cos(t)*radius,sin(t)*radius)+cat.to_global(Vector2(0,0))
